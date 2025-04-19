d945a307dbea806bad225d5d5a9f5630a5f64098
Dingle <Stepa202120212021@gmail.com>
2025-04-18 19:54:35 -0600

    Initial Commit

---
+++ HalloWorld.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+    // Variables
+    int x = 2432410;
+    int y = 14145;
+    int sum = x + y;
+
+    std::string stringy = "I'm a string!";
+
+    // Printing
+    std::cout << "X: " << x << "\n";
+    std::cout << "Y: " << y << "\n";
+    std::cout << "Sum: " << sum << "\n";
+    std::cout << stringy;
+
+    return 0;
+}
